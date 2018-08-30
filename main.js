17ca9102c704ea07fe5765f87d2c91d5a80a6b94
Beau Spencer <bdspencer416@gmail.com>
2018-08-30 11:21:37 -0700

    changed color scheme of button to match better, modified main.js to remove redundancy and give better name to start stop button. #2

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-var $button = document.getElementById('button')
+var $stopStartButton = document.getElementById('button')
 var $elapsed = document.getElementById('elapsedtime')
 var timerID
 
 function timer(){
-  if($button.classList.contains('start') === true){
-    $button.classList.remove('start')
-    $button.classList.add('stop')
-    $button.textContent = 'Stop'
+  if($stopStartButton.classList.contains('start')){
+    $stopStartButton.classList.remove('start')
+    $stopStartButton.classList.add('stop')
+    $stopStartButton.textContent = 'Stop'
     timerID = setInterval(function() {
       $elapsed.textContent++
     }, 1000)
   }
   else {
-    $button.classList.remove('stop')
-    $button.classList.add('start')
-    $button.textContent = 'Start'
+    $stopStartButton.classList.remove('stop')
+    $stopStartButton.classList.add('start')
+    $stopStartButton.textContent = 'Start'
     clearInterval(timerID)
   }
 }
 
-$button.addEventListener('click', timer)
+$stopStartButton.addEventListener('click', timer)
