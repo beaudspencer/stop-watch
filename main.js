e264f94dc7f1d3c8a8aa868d3ac2a5108e5a7790
Beau Spencer <bdspencer416@gmail.com>
2018-08-30 10:23:47 -0700

    modified index.html to contain class to track button. modified main.js to change class to track state of timer. adjusted styles.css to maintain current look with new labeling

---
+++ main.js
@@ -0,0 +1,21 @@
+var $button = document.getElementById('button')
+var $elapsed = document.getElementById('elapsedtime')
+var timerID
+
+function timer(){
+  if($button.classList.contains('start') === true){
+    $button.classList.remove('start')
+    $button.classList.add('stop')
+    $button.textContent = 'Stop'
+    timerID = setInterval(function() {
+      $elapsed.textContent++
+    }, 1000)
+  }
+  else {
+    $button.classList.remove('stop')
+    $button.classList.add('start')
+    $button.textContent = 'Start'
+  }
+}
+
+$button.addEventListener('click', timer)
